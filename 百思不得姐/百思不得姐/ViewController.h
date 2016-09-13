a9ac4b6d90a7250338139a46c76e1876f2a1c4ae
benjamin <benjamin_cly@qq.com>
2016-09-13 17:10:38 +0800

    初始化项目

---
+++ 百思不得姐/百思不得姐/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思不得姐
+//
+//  Created by Lucius on 16/9/13.
+//  Copyright © 2016年 Benjamin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by Lucius on 16/9/13.
+//  Copyright © 2016年 Benjamin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
